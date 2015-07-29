eda1691b62876137f4c9b3d06d0fe02a92b8cbbe
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2015-07-29 22:11:24 +0900

    add documentation for all slots of conditions.

--- src/error.lisp
+++ src/error.lisp
@@ -1,88 +1,83 @@
 (in-package :cl-user)
 (defpackage clipper.error
-  (:use :cl
-        :annot.doc))
+  (:use :cl))
 (in-package :clipper.error)
 
 (syntax:use-syntax :annot)
 
-@doc
-"Base error condition."
 @export
-(define-condition <clipper-error> (simple-error) ())
+(define-condition <clipper-error> (simple-error)
+  ()
+  (:documentation "Base error condition."))
 
-@doc
-"Invalid stare type error."
 @export
 (define-condition <clipper-invalid-store-type> (<clipper-error>)
-  ((type :initarg :type))
+  ((type :initarg :type :documentation "Type slot."))
   (:report
    (lambda (condition stream)
      (format stream
              "Invalid store type: ~a."
-             (slot-value condition 'type)))))
+             (slot-value condition 'type))))
+  (:documentation "Invalid stare type error."))
 
-@doc
-"Incomplete config error."
 @export
 (define-condition <clipper-incomplete-config> (<clipper-error>)
-  ((slot-list :initarg :slot-list))
+  ((slot-list :initarg :slot-list :documentation "Invalid slot list slot."))
   (:report
    (lambda (condition stream)
      (format stream
              "Incomplete config: ~{~A ~}"
-             (slot-value condition 'slot-list)))))
+             (slot-value condition 'slot-list))))
+  (:documentation "Incomplete config error."))
 
-@doc
-"Incomplete local config error."
 @export
-(define-condition <clipper-incomplete-local-config> (<clipper-incomplete-config>) ())
+(define-condition <clipper-incomplete-local-config> (<clipper-incomplete-config>)
+  ()
+  (:documentation "Incomplete local config error."))
 
-@doc
-"Incomplete S3 config error."
 @export
-(define-condition <clipper-incomplete-s3-config> (<clipper-incomplete-config>) ())
+(define-condition <clipper-incomplete-s3-config> (<clipper-incomplete-config>)
+  ()
+  (:documentation "Incomplete S3 config error."))
 
-@doc
-"No source specified error."
 @export
 (define-condition <clipper-no-source-specified> (<clipper-error>)
-  ((object :initarg :object))
+  ((object :initarg :object :documentation "Object slot."))
   (:report
    (lambda (condition stream)
      (format stream
              "Please provide URL or vector data with ~a."
-             (slot-value condition 'object)))))
+             (slot-value condition 'object))))
+  (:documentation "No source specified error."))
 
-@doc
-"Unsupported content type error."
 @export
 (define-condition <clipper-unsupported-content-type> (<clipper-error>)
-  ((content-type :initarg :content-type))
+  ((content-type :initarg :content-type :documentation "Content type slot."))
   (:report
    (lambda (condition stream)
      (format stream
              "Unsupported content-type : ~a."
-             (slot-value condition 'content-type)))))
+             (slot-value condition 'content-type))))
+  (:documentation "Unsupported content type error."))
 
 @export
 (define-condition <clipper-incomplete-for-attach-image> (<clipper-error>)
-  ((type :initarg :type)
-   (args :initarg :args))
+  ((type :initarg :type :documentation "Type slot.")
+   (args :initarg :args :documentation "Args slot."))
   (:report
    (lambda (condition stream)
      (format stream
              "attach-image with ~a needs~{ ~a~}"
              (slot-value condition 'type)
-             (slot-value condition 'args)))))
+             (slot-value condition 'args))))
+  (:documentation "Incomplete for attach-image."))
 
-@doc
-"Image type error."
 @export
 (define-condition <clipper-image-type-error> (<clipper-error>)
-  ((type :initarg :type))
+  ((type :initarg :type :documentation "Type slot."))
   (:report
    (lambda (condition stream)
      (format stream
              "Type of image: ~a does not satisfy '(simple-array (unsigned-byte 8))."
-             (slot-value condition 'type)))))
+             (slot-value condition 'type))))
+  (:documentation "Image type error."))

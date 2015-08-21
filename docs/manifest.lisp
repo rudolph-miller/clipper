fb40e5c718fe93b2f2ae6b04f655c740b77e827b
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2015-08-21 02:09:48 +0000

    update docs at 1440122988. [ci skip]

---
+++ docs/manifest.lisp
@@ -0,0 +1,7 @@
+(:docstring-markup-format :scriba
+ :systems (:clipper)
+ :documents ((:title "CLIPPER"
+              :authors ("Rudolph-Miller")
+              :output-format (:type :multi-html
+                              :template :minima)
+              :sources ("manual.scr"))))
